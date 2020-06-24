f75e7edfc96688f188c70ec4be5bb2250387c565
Darian Tavana <dtavana@fb.com>
2020-06-24 18:21:50 -0400

    Add MovieTrailerActivity

--- app/src/main/java/com/dtavana/flixter/MovieDetailsActivity.java
+++ app/src/main/java/com/dtavana/flixter/MovieDetailsActivity.java
@@ -2,19 +2,33 @@ package com.dtavana.flixter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.codepath.asynchttpclient.AsyncHttpClient;
+import com.codepath.asynchttpclient.RequestParams;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
 import com.dtavana.flixter.databinding.ActivityMovieDetailsBinding;
 import com.dtavana.flixter.models.Movie;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.parceler.Parcels;
+
+import okhttp3.Headers;
 
 public class MovieDetailsActivity extends AppCompatActivity {
 
     public static final String TAG = "MovieDetailsActivity";
+    public static final String VIDEO_URL = "https://api.themoviedb.org/3/movie/%d/videos";
+    public static final String KEY_VIDEO_ID = "videoId";
 
     Movie movie;
 
@@ -31,5 +45,46 @@ public class MovieDetailsActivity extends AppCompatActivity {
         binding.tvOverview.setText(movie.getOverview());
 
         binding.rbVoteAverage.setRating((float) (movie.getVoteAverage() / 2));
+
+        binding.getRoot().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AsyncHttpClient client = new AsyncHttpClient();
+                RequestParams p = new RequestParams();
+                p.put("api_key", getString(R.string.MOVIE_DB_API_KEY));
+
+                if (movie.videoId == null) {
+                    client.get(String.format(VIDEO_URL, movie.id), p, new JsonHttpResponseHandler() {
+                        @Override
+                        public void onSuccess(int statusCode, Headers headers, JSON json) {
+                            Log.d(TAG, "Video onSuccess");
+                            JSONObject jsonObject = json.jsonObject;
+                            try {
+                                JSONArray results = jsonObject.getJSONArray("results");
+                                Log.i(TAG, "Video JSON Result: " + jsonObject.toString());
+                                String videoId = ((JSONObject) results.get(0)).getString("key");
+                                movie.setVideoId(videoId);
+                                Intent i = new Intent(MovieDetailsActivity.this, MovieTrailerActivity.class);
+                                i.putExtra(KEY_VIDEO_ID, videoId);
+                                startActivity(i);
+                            } catch (JSONException e) {
+                                Log.e(TAG, "Hit JSON exception", e);
+                            }
+
+                        }
+
+                        @Override
+                        public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+                            Log.d(TAG, "Video onFailure");
+                        }
+                    });
+                }
+                else {
+                    Intent i = new Intent(MovieDetailsActivity.this, MovieTrailerActivity.class);
+                    i.putExtra(KEY_VIDEO_ID, movie.videoId);
+                    startActivity(i);
+                }
+            }
+        });
     }
 }
