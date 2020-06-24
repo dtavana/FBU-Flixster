81d97ab56efcf27aa7fba4293e6ea5d92f99176e
Darian Tavana <dtavana@fb.com>
2020-06-24 12:52:09 -0400

    Add Movie model

---
+++ app/src/main/java/com/dtavana/flixter/models/Movie.java
@@ -0,0 +1,40 @@
+package com.dtavana.flixter.models;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Movie {
+    String posterPath;
+    String title;
+    String overview;
+
+    public Movie(JSONObject obj) throws JSONException {
+        this.posterPath = obj.getString("poster_path");
+        this.title = obj.getString("title");
+        this.overview = obj.getString("overview");
+    }
+
+    public static List<Movie> fromJsonArray(JSONArray arr) throws JSONException {
+        List<Movie> res = new ArrayList<>();
+        for(int i = 0; i < arr.length(); i++) {
+            res.add(new Movie(arr.getJSONObject(i)));
+        }
+        return res;
+    }
+
+    public String getPosterPath() {
+        return String.format("https://image.tmdb.org/t/p/w342%s", posterPath);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+}
