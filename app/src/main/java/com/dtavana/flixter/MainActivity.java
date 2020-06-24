e6912a7514702b0a1084d08f8e1f17cef482fd1c
Darian Tavana <dtavana@fb.com>
2020-06-24 13:51:12 -0400

    Move 'api_key' to secrets.xml

--- app/src/main/java/com/dtavana/flixter/MainActivity.java
+++ app/src/main/java/com/dtavana/flixter/MainActivity.java
@@ -1,25 +1,30 @@
 package com.dtavana.flixter;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
 
 import com.codepath.asynchttpclient.AsyncHttpClient;
+import com.codepath.asynchttpclient.RequestParams;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+import com.dtavana.flixter.adapters.MovieAdapter;
 import com.dtavana.flixter.models.Movie;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import okhttp3.Headers;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String NOW_PLAYING_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
+    public static final String NOW_PLAYING_URL = "https://api.themoviedb.org/3/movie/now_playing";
     public static final String TAG = "MainActivity";
 
     List<Movie> movies;
